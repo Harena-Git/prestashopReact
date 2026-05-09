18afab77d7da6d69d1ef0ec72a4b376424da8b97
Harena-Git <faniriantsoaharena@gmail.com>
2026-05-09 20:43:46 +0300

    Update file

--- src/features/modules/components/ModuleSelectionList.jsx
+++ src/features/modules/components/ModuleSelectionList.jsx
@@ -1,39 +1,67 @@
+/**
+ * FONCTION: ModuleSelectionList
+ * ANJARA: Mampiseho lisitra iray misy anarana modules ary misy boaty kely (checkbox) eo anilany.
+ *       Ahafahana mifantina modules maromaro indray miaraka.
+ */
 function ModuleSelectionList({
-  modules,
-  selectedModules,
-  loading,
-  allSelected,
-  onToggleAll,
-  onToggleModule,
+  modules,           // Lisitry ny modules rehetra azo isafidianana.
+  selectedModules,   // Ireo modules efa voafantina.
+  loading,           // Mampiseho raha mbola misy asa mandeha (ohatra: maka modules).
+  allSelected,       // Marina (true) raha voafantina daholo ny modules rehetra.
+  onToggleAll,       // Asa (fonction) antsoina rehefa tsindriana ilay checkbox "Tout sélectionner".
+  onToggleModule,    // Asa (fonction) antsoina rehefa tsindriana ny checkbox-n'ny module iray.
 }) {
+  
+  // Ny ampahany hita maso (HTML/JSX) izay avoakan'ity fonction ity.
   return (
     <>
-      {/* checkbox pour selectionner tout les elements */}
+      {/* Fizarana 1: Checkbox an'ny "Tout sélectionner" */}
       <div style={{ marginBottom: 12 }}>
         <label>
           <input
             type="checkbox"
+            // Hamarinina raha efa voafantina daholo ny modules rehetra
             checked={allSelected}
+            // Rehefa tsindriana ity checkbox ity dia antsoina ny fonction onToggleAll
             onChange={onToggleAll}
+            // Tsy azo tsindriana ity checkbox ity raha mbola misy asa mandeha (loading)
             disabled={loading}
-          />{" "}
+          />
+          {" "}
           Tout selectionner
         </label>
       </div>
 
-      <ul style={{ listStyle: "none", padding: 0, margin: 0, maxHeight: 420, overflowY: "auto" }}>
+      {/* Fizarana 2: Lisitry ny modules tsirairay */}
+      <ul style={{ 
+        listStyle: "none",  // Tsy asiana teboka na isa eo alohan'ny lisitra
+        padding: 0,         // Tsy asiana sisiny anatiny
+        margin: 0,          // Tsy asiana sisiny ivelany
+        maxHeight: 420,     // Ny haavony be indrindra dia 420px
+        overflowY: "auto"   // Aseho ny "ascenseur" raha mihoatra ny haavony ny lisitra
+      }}>
+        
+        {/* Mamerina mampiseho isaky ny module ao anatin'ny lisitry ny 'modules' */}
         {modules.map((moduleName) => (
+          
+          // Isaky ny module dia mamboatra an'ity singa 'li' ity
           <li key={moduleName} style={{ marginBottom: 8 }}>
             <label>
               <input
                 type="checkbox"
+                // Voamarika (checked) ity raha toa ka ao anatin'ny lisitry ny 'selectedModules' ilay module
                 checked={selectedModules.includes(moduleName)}
+                // Rehefa tsindriana dia antsoina ny fonction onToggleModule miaraka amin'ny anaran'ilay module
                 onChange={() => onToggleModule(moduleName)}
+                // Tsy azo tsindriana raha mbola misy asa mandeha (loading)
                 disabled={loading}
-              />{" "}
+              />
+              {" "}
+              {/* Aseho eto ny anaran'ilay module */}
               {moduleName}
             </label>
           </li>
+          
         ))}
       </ul>
     </>
--- src/features/modules/utils/selection.js
+++ src/features/modules/utils/selection.js
@@ -1,11 +1,27 @@
+/**
+ * Manova ny fifantenana ny module iray.
+ * Raha efa voafantina ilay module dia esorina amin'ny lisitra, raha tsy izany dia ampiana.
+ * @param {string[]} selectedModules - Ny lisitry ny modules efa voafantina.
+ * @param {string} moduleName - Ny anaran'ny module hovaina ny fifantenana azy.
+ * @returns {string[]} Ny lisitra vaovao misy ny modules voafantina.
+ */
 export function toggleModuleSelection(selectedModules, moduleName) {
-  if (selectedModules.includes(moduleName)) {
-    return selectedModules.filter((name) => name !== moduleName);
-  }
+  // Hamarinina raha toa ka efa ao anatin'ny lisitry ny voafantina ilay module
+  const isSelected = selectedModules.includes(moduleName);
 
-  return [...selectedModules, moduleName];
+  // Raha efa voafantina izy, dia esorina. Raha tsy izany, dia ampiana.
+  return isSelected
+    ? selectedModules.filter((name) => name !== moduleName)
+    : [...selectedModules, moduleName];
 }
 
+/**
+ * Manamarina raha voafantina daholo ny modules rehetra.
+ * @param {string[]} selectedModules - Ny lisitry ny modules efa voafantina.
+ * @param {string[]} allModules - Ny lisitry ny modules rehetra misy.
+ * @returns {boolean} True raha voafantina daholo, false raha tsy izany.
+ */
 export function areAllModulesSelected(selectedModules, allModules) {
+  // Hamarinina raha toa ka misy modules ary mitovy ny isan'ny voafantina sy ny isan'ny modules rehetra
   return allModules.length > 0 && selectedModules.length === allModules.length;
 }
